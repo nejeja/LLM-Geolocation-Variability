106d424d0ba1cc9d079f9dbc489f5da49136401b
nejeja <160975120+nejeja@users.noreply.github.com>
2025-08-31 15:08:35 +0200

    Delete cycle.sh

--- cycle.sh
+++
@@ -1,9 +0,0 @@
-# nastavíš seznam uzlů, co máš definované v case mapě
-NODES=(vpn-eu-1 vpn-us-1 vpn-br-1 vpn-cn-1 vpn-ir-1 vpn-ru-1)
-
-# a pak smyčku
-for n in "${NODES[@]}"; do
-  echo "=== Switching to $n ==="
-  ./vpn_switch.sh "$n"
-  sleep 10   # pauza 10 sekund mezi přepnutími
-done
